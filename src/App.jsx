f1a4890c836fb1868b19e0ad6a50216e6edaec15
Daniel Faro <danielfaro@daniels-mbp.lan>
2023-11-14 21:50:13 -0800

    removed place-content center from App css

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ const Dashboard = styled.div`
   display: grid;
   grid-template-rows: 1fr 4fr 1fr 10fr;
   overflow: hidden;
-  /* place-content: center; */
   text-align: center;
   width: 100%;
   height: 100vh;
